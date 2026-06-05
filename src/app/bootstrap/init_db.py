f3542b76f1fb8d69d14bfa32d5b2c73275d8a719
Lemorita95 <leandronmorita@gmail.com>
2026-06-05 15:11:58 +0200

    fix: improve image security and add .dockignore

--- src/app/bootstrap/init_db.py
+++ src/app/bootstrap/init_db.py
@@ -1,9 +1,9 @@
 from sqlmodel import SQLModel
-from app.db.connection import admin_engine
+from app.db.connection import engine
 import app.db.model 
 
 def init_db():
-    SQLModel.metadata.create_all(admin_engine)
+    SQLModel.metadata.create_all(engine)
 
 if __name__ == '__main__':
     init_db()
